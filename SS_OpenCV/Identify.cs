432d10f05b5967849257ad7680c32e536a4ce3ec
Filip Szymański <szymanski996@gmail.com>
2019-12-14 19:11:11 +0000

    connected algorithm prototype

--- SS_OpenCV/Identify.cs
+++ SS_OpenCV/Identify.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Emgu.CV.Structure;
 using Emgu.CV;
+using System.Linq;
 
 
 namespace SS_OpenCV
@@ -61,5 +62,164 @@ namespace SS_OpenCV
             }
         }
 
+        struct Coords
+        {
+            int code;
+            int begX;
+            int begY;
+            int endX;
+            int endY;
+            int frameHeight;
+            int frameWidth;
+        }
+
+
+        public static void connectedComponents(Image<Bgr, byte> img, Image<Bgr, byte> imgCopy, int hueLimit  =  20, int satLimit  =  50, int valLimit =  50)
+        {
+            unsafe
+            {
+                int x, y;
+                MIplImage m = img.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+
+                MIplImage m2 = imgCopy.MIplImage;
+                byte* dataPtr2 = (byte*)m2.imageData.ToPointer(); // Pointer to the image
+
+                int width = img.Width;
+                int height = img.Height;
+                int nChan = m.nChannels; // number of channels - 3
+                int padding = m.widthStep - m.nChannels * m.width;
+
+                int hue = 0;
+                int value = 0;
+                int saturation = 0;
+
+                int[, ] indexTable = new int[width, height];
+                int currentMax = 0;
+                int toChange = -1;
+                List<int> objects = new List<int>();
+                List<Coords> result = new List<Coords>();
+
+                for (y = 1; y < height-1; y++)
+                {
+                    for (x = 1; x < width-1; x++)
+                    {
+                        value = ((dataPtr2 + y * m.widthStep + x * nChan)[0] * 100) / 255;
+                        saturation = ((dataPtr2 + y * m.widthStep + x * nChan)[1] * 100) / 255;
+                        hue = ((dataPtr2 + y * m.widthStep + x * nChan)[2] * 360 ) / 255;
+
+                        if (hue >= 0 && hue <=hueLimit)         //if red
+                        {
+                            //8 - connectivity
+                            if (indexTable[x + 1, y - 1] != 0)
+                                indexTable[x, y] = indexTable[x + 1, y - 1];
+
+                            if (indexTable[x, y - 1] != 0 && indexTable[x, y] != indexTable[x, y - 1])
+                            {
+                                if(indexTable[x, y] == 0)
+                                    indexTable[x, y] = indexTable[x, y - 1];
+                                else                  //Find lower index,  change  higher  to lower
+                                {
+                                    if (indexTable[x, y] < indexTable[x, y - 1])
+                                        toChange = indexTable[x, y - 1];
+
+                                    else
+                                    {
+                                        indexTable[x, y] = indexTable[x, y - 1];
+                                        toChange = indexTable[x, y];
+                                    }
+                                    for (int i = 0; i < height; i++)
+                                    {
+                                        for (int j = 0; j < width; j++)
+                                        {
+                                            if (indexTable[i, j] == toChange)
+                                                indexTable[i, j] = toChange;
+                                        }
+                                    }
+                                    objects.Remove(objects.Single(r => r == toChange));
+                                }
+                            }
+                            if (indexTable[x - 1,  y  - 1] != 0 && indexTable[x, y] != indexTable[x - 1,  y  - 1])
+                            {
+                                if (indexTable[x, y] == 0)
+                                    indexTable[x, y] = indexTable[x - 1,  y  - 1];
+                                else
+                                {
+                                    if (indexTable[x, y] < indexTable[x - 1,  y  - 1])
+                                        toChange = indexTable[x - 1,  y  - 1];
+
+                                    else
+                                    {
+                                        indexTable[x, y] = indexTable[x - 1,  y  - 1];
+                                        toChange = indexTable[x, y];
+                                    }
+                                    for (int i = 0; i < height; i++)
+                                    {
+                                        for (int j = 0; j < width; j++)
+                                        {
+                                            if (indexTable[i, j] == toChange)
+                                                indexTable[i, j] = toChange;
+                                        }
+                                    }
+                                    objects.Remove(objects.Single(r => r == toChange));
+                                }
+                            }
+                            if (indexTable[x - 1,  y ] != 0 && indexTable[x, y] != indexTable[x - 1,  y ])
+                            {
+                                if (indexTable[x, y] == 0)
+                                    indexTable[x, y] = indexTable[x - 1,  y ];
+                                else
+                                {
+                                    if (indexTable[x, y] < indexTable[x - 1,  y ])
+                                        toChange = indexTable[x - 1,  y ];
+
+                                    else
+                                    {
+                                        indexTable[x, y] = indexTable[x - 1,  y ];
+                                        toChange = indexTable[x, y];
+                                    }
+                                    for (int i = 0; i < height; i++)
+                                    {
+                                        for (int j = 0; j < width; j++)
+                                        {
+                                            if (indexTable[i, j] == toChange)
+                                                indexTable[i, j] = toChange;
+                                        }
+                                    }
+                                    objects.Remove(objects.Single(r => r == toChange));
+                                }
+                            }
+                            else
+                            {
+                                currentMax = currentMax++;
+                                indexTable[x, y] = currentMax;
+                                objects.Add(currentMax);
+                            }
+                        }
+                        else
+                        {
+                            indexTable[x, y] = 0;
+                        }                     
+                    }
+                }
+                foreach (int obj in objects)
+                {
+                    int code;
+                    int begX;
+                    int begY;
+                    int endX;
+                    int endY;
+                    int frameHeight;
+                    int frameWidth;
+                    for (int i = 0; i < height; i++)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            if ()
+                    }
+                    }
+                }
+            }
+        }
     }
 }
