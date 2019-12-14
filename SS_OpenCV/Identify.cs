72e622c2a9de17d54c0c34ac55308208e1f7d616
Filip Szymański <szymanski996@gmail.com>
2019-12-14 19:12:36 +0000

    connected algorithm prototype

--- SS_OpenCV/Identify.cs
+++ SS_OpenCV/Identify.cs
@@ -10,13 +10,14 @@ namespace SS_OpenCV
 {
     class Identify
     {
-        public static void BgrToHsv(Image<Bgr, byte> img, Image<Bgr, byte> imgCopy)
+        public static void SignIdentify(Image<Bgr, byte> img, Image<Bgr, byte> imgCopy)
         {
             unsafe
             {
                 int x, y;
                 MIplImage m = img.MIplImage;
                 byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+
                 MIplImage m2 = imgCopy.MIplImage;
                 byte* dataPtr2 = (byte*)m2.imageData.ToPointer(); // Pointer to the image
 
@@ -24,6 +25,18 @@ namespace SS_OpenCV
                 int height = img.Height;
                 int nChan = m.nChannels; // number of channels - 3
                 int padding = m.widthStep - m.nChannels * m.width;
+                int[] sign_coords = new int[4];
+                /*
+                sign_coords[0] = Left-x
+                sign_coords[1] = Top-y
+                sign_coords[2] = Right-x
+                sign_coords[3] = Bottom-y
+                */
+
+                sign_coords[0] = 200;
+                sign_coords[1] = 300;
+                sign_coords[2] = 400;
+                sign_coords[3] = 500;
 
                 for (y = 0; y < height; y++)
                 {
@@ -32,81 +45,11 @@ namespace SS_OpenCV
                         var x0 = x;
                         var y0 = y;
 
-                        int blue = (dataPtr + y * m.widthStep + x * nChan)[0];
-                        int green = (dataPtr + y * m.widthStep + x * nChan)[1];
-                        int red = (dataPtr + y * m.widthStep + x * nChan)[2];
-
-                        System.Drawing.Color intermediate = System.Drawing.Color.FromArgb(red, green, blue);                        
-                        Hsv hsvPixel = new Hsv(intermediate.GetHue(), intermediate.GetSaturation(), intermediate.GetBrightness());
-
-                        double r = red/255.0;
-                        double g = green/255.0;
-                        double b = blue/255.0;
-                        double hue, sat, val;
-
-                        double cmax = Math.Max(r,b);
-                        cmax = Math.Max(cmax,g);
-                        double cmin = Math.Min(r, b);
-                        cmin = Math.Min(cmin, g);
-                        double delta = cmax - cmin;
-
-                        //Hue
-                        hue = hsvPixel.Hue;
-
-                        //Saturation
-                        if (cmax == 0)
-                        {
-                            sat = 0;
-                        }
-                        else
-                        {
-                            sat = delta / cmax;
-                        }
-
-                        //Value
-                        val = cmax;
-
-                        (dataPtr2 + y * m.widthStep + x * nChan)[0] = (byte)(Math.Round(val*255));
-                        (dataPtr2 + y * m.widthStep + x * nChan)[1] = (byte)(Math.Round(sat*255));
-                        (dataPtr2 + y * m.widthStep + x * nChan)[2] = (byte)(Math.Round(hue*255/360));
-                    }
-                }
-            }
-        }
-
-        public static void DrawRectangle(Image<Bgr, byte> img, int[] sign_coords)
-        {
-            unsafe
-            {
-                int x, y;
-                MIplImage m = img.MIplImage;
-                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
-
-                int width = img.Width;
-                int height = img.Height;
-                int nChan = m.nChannels; // number of channels - 3
-                int padding = m.widthStep - m.nChannels * m.width;
-                //int[] sign_coords = new int[4];
-                /*
-                sign_coords[0] = Left-x
-                sign_coords[1] = Top-y
-                sign_coords[2] = Right-x
-                sign_coords[3] = Bottom-y
-                */
-                /*
-                sign_coords[0] = 200;
-                sign_coords[1] = 300;
-                sign_coords[2] = 400;
-                sign_coords[3] = 500;
-                */
-                for (y = 0; y < height; y++)
-                {
-                    for (x = 0; x < width; x++)
-                    {
-                        //Drawing rectangle
                         if (
-                            x > sign_coords[0] && x < sign_coords[2] && (y == sign_coords[1] || y == sign_coords[3]) || //vertical lines
-                            y > sign_coords[1] && y < sign_coords[3] && (x == sign_coords[0] || x == sign_coords[2])    //horizontal lines
+                            //x0 > 200 && x0 < 400 && (y0 == 300 || y0 == 500) || //vertical lines
+                            //y0 > 300 && y0 < 500 && (x0 == 200 || x0 == 400)    //horizontal lines
+                            x0 > sign_coords[0] && x0 < sign_coords[2] && (y0 == sign_coords[1] || y0 == sign_coords[3]) || //vertical lines
+                            y0 > sign_coords[1] && y0 < sign_coords[3] && (x0 == sign_coords[0] || x0 == sign_coords[2])    //horizontal lines
                             )
                         {
                             // get pixel address
@@ -121,13 +64,24 @@ namespace SS_OpenCV
 
         struct Coords
         {
-            int code;
-            int begX;
-            int begY;
-            int endX;
-            int endY;
-            int frameHeight;
-            int frameWidth;
+            private int code;
+            private int begX;
+            private int begY;
+            private int endX;
+            private int endY;
+            private int frameHeight;
+            private int frameWidth;
+
+            public Coords(int code, int begX, int begY, int endX, int endY, int frameHeight, int frameWidth) : this()
+            {
+                this.code = code;
+                this.begX = begX;
+                this.begY = begY;
+                this.endX = endX;
+                this.endY = endY;
+                this.frameHeight = frameHeight;
+                this.frameWidth = frameWidth;
+            }
         }
 
 
@@ -261,20 +215,35 @@ namespace SS_OpenCV
                 }
                 foreach (int obj in objects)
                 {
-                    int code;
-                    int begX;
-                    int begY;
-                    int endX;
-                    int endY;
+                    int code = obj;
+                    int begX = 10000000;
+                    int begY = 10000000;
+                    int endX = -1;
+                    int endY = -1;
                     int frameHeight;
                     int frameWidth;
                     for (int i = 0; i < height; i++)
                     {
                         for (int j = 0; j < width; j++)
                         {
-                            if ()
+                            if(indexTable[i, j] == code)
+                            {
+                                if (i < begX)
+                                    begX = i;
+                                if (j < begY)
+                                    begY = i;
+                                if (i > endX)
+                                    endX = i;
+                                if (j > endY)
+                                    endY = j;
+                            }
+                        }
                     }
-                    }
+
+                    frameWidth = endX - begX;
+                    frameHeight = endY - begY;
+
+                    result.Add(new Coords(code, begX, begY, endX, endY, frameHeight, frameWidth));
                 }
             }
         }
